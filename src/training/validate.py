99b3f92a93feb14dc1fe9dab3b77653210e62afa
m-k-julkarnain <mkjulkarnain.akash@gmail.com>
2026-09-20 22:11:11 +0600

    Make validation MAE order-invariant to prevent fake inflated errors

--- src/training/validate.py
+++ src/training/validate.py
@@ -1,4 +1,4 @@
-""" Validation script for Fetal Head Biometry HQNN. """
+""" Validation script for Fetal Head Biometry HQNN (Order-Invariant). """
 import torch
 import numpy as np
 from tqdm.auto import tqdm
@@ -28,9 +28,18 @@ def validate(model, dataloader, criterion, device):
     preds_arr = ((np.array(preds_list) + 1.0) / 2.0) * IMAGE_SIZE
     targets_arr = ((np.array(targets_list) + 1.0) / 2.0) * IMAGE_SIZE
     
-    # Evaluate BPD (cols 0-3) and OFD (cols 4-7)
-    mae_bpd = np.mean(np.abs(preds_arr[:, :4] - targets_arr[:, :4]), axis=1)
-    mae_ofd = np.mean(np.abs(preds_arr[:, 4:] - targets_arr[:, 4:]), axis=1)
+    # --- Order-Invariant MAE for BPD ---
+    mae_bpd_straight = np.mean(np.abs(preds_arr[:, :4] - targets_arr[:, :4]), axis=1)
+    targets_bpd_flipped = np.concatenate([targets_arr[:, 2:4], targets_arr[:, 0:2]], axis=1)
+    mae_bpd_flipped = np.mean(np.abs(preds_arr[:, :4] - targets_bpd_flipped), axis=1)
+    mae_bpd = np.minimum(mae_bpd_straight, mae_bpd_flipped)
+    
+    # --- Order-Invariant MAE for OFD ---
+    mae_ofd_straight = np.mean(np.abs(preds_arr[:, 4:] - targets_arr[:, 4:]), axis=1)
+    targets_ofd_flipped = np.concatenate([targets_arr[:, 6:8], targets_arr[:, 4:6]], axis=1)
+    mae_ofd_flipped = np.mean(np.abs(preds_arr[:, 4:] - targets_ofd_flipped), axis=1)
+    mae_ofd = np.minimum(mae_ofd_straight, mae_ofd_flipped)
+    
     true_mae_per_image = (mae_bpd + mae_ofd) / 2.0
     mean_mae = np.mean(true_mae_per_image)
     
